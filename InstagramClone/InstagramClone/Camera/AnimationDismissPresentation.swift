d9e174a82dbed840447dc9cc448b0617093e375a
Alper Us <alper.usss@gmail.com>
2023-06-17 15:44:24 -0700

    Camera

---
+++ InstagramClone/InstagramClone/Camera/AnimationDismissPresentation.swift
@@ -0,0 +1,8 @@
+//
+//  AnimationDismissPresentation.swift
+//  InstagramClone
+//
+//  Created by Alper Us on 2023-06-17.
+//
+
+import Foundation
---
+++ InstagramClone/InstagramClone/Camera/AnimationPresentation.swift
@@ -0,0 +1,8 @@
+//
+//  AnimationPresentation.swift
+//  InstagramClone
+//
+//  Created by Alper Us on 2023-06-17.
+//
+
+import Foundation
