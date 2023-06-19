a367594bafc9e31c7ffacc4b81dded9a5299dd62
Alper Us <alper.usss@gmail.com>
2023-06-18 17:27:31 -0700

    Added Comment and Like

---
+++ InstagramClone/InstagramClone/MainPage/CommentCell.swift
@@ -0,0 +1,8 @@
+//
+//  CommentCell.swift
+//  InstagramClone
+//
+//  Created by Alper Us on 2023-06-18.
+//
+
+import Foundation
---
+++ InstagramClone/InstagramClone/Model/Comment.swift
@@ -0,0 +1,8 @@
+//
+//  Comment.swift
+//  InstagramClone
+//
+//  Created by Alper Us on 2023-06-18.
+//
+
+import Foundation
